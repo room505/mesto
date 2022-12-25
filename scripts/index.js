15878729cf830ea2b1798772920dcec1e36506bf
Denis Syromiatnikov <dsyromiatnikov@yandex.ru>
2022-12-25 21:17:02 +0300

    частичное исправление после 1-го ревью

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+let author = document.querySelector(".profile__author");
+let aboutTheAutor = document.querySelector(".profile__about-the-author");
+let editButton = document.querySelector(".profile__button-edit");
+
+let popup = document.querySelector(".popup");
+let popupForm = document.querySelector(".popup__container");
+let closePopup = popup.querySelector(".popup__close");
+let saveEdit = popup.querySelector(".popup__save-edit");
+let renameAuthor = popup.querySelector(".popup__text-imput_edit_author");
+let editAboutTheAuthor = popup.querySelector(
+  ".popup__text-input_edit_about-the-author"
+);
+
+function closePop() {
+  popup.classList.toggle("popup_hidden");
+}
+
+closePop();
+
+editButton.addEventListener("click", function openPop() {
+  popup.classList.remove("popup_hidden");
+  renameAuthor.value = author.textContent;
+  editAboutTheAuthor.value = aboutTheAutor.textContent;
+});
+
+closePopup.addEventListener("click", closePop);
+
+function saveEditProfile(evt) {
+  evt.preventDefault();
+  author.textContent = renameAuthor.value;
+  aboutTheAutor.textContent = editAboutTheAuthor.value;
+  closePop();
+}
+
+popupForm.addEventListener("submit", saveEditProfile);
